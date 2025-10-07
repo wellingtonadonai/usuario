9b6aff6a416960b71ccc78dde55a6d535c67c959
wellington <wellington.h.s.oliveira@hotmail.com>
2025-10-07 19:05:19 -0300

    mudanças

--- src/main/java/br/com/wellington/business/dto/UsuarioDto.java
+++ src/main/java/br/com/wellington/business/dto/UsuarioDto.java
@@ -13,7 +13,6 @@ public class UsuarioDto {
     private String nome;
     private String email;
     private String senha;
-
     private List<EnderecoDto> enderecos;
     private List<TelefoneDto> telefone;
 }
